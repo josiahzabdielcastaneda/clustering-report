6706c8fd2a252805f7dbfa4b9c3d90e6efa7a3b7
josia <josiahzabdiel.castaneda@cit.edu>
2024-04-22 17:24:28 +0800

    changed output of the streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,16 @@ if(st.button("Predict Cluster")):
         spending_score = float(input_spendingScore)
         #annual_income = ScaleAnnualIncome(annual_income)
         cluster = predict_cluster(annual_income, spending_score)
-        st.write(f"This customer belongs to Cluster {cluster+1}")
+        st.write(f"This customer belongs to Cluster {cluster+1}\n")
+        if cluster == 0:
+            st.write("Customer has a middling annual income and spending score. They are a potential target customer.")
+        elif cluster == 1:
+            st.write("Customer has high income and spending score. Continue targeting this customer.")
+        elif cluster == 2:
+            st.write("Customer has high annual income but low spending score. Consider appealing to their needs and wants")
+        elif cluster == 3:
+            st.write("Customer has low annual income and low spending score.")
+        elif cluster == 4:
+            st.write("Customer has low income but high spending score. Continue targeting this customer.")
     except ValueError:
         st.error("Please enter valid numerical values for the Spending Score and Annual Income")
